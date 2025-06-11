0c9eee35ec3ad76d8e24ea2ce367f432238b8339
ABHISHEK KUMAR SINGH <abhisinghaks12@gmail.com>
2025-06-11 22:37:57 +0530

    Initial commit - backend

---
+++ server/config/routes/auth.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const existing = await User.findOne({ email });
+    if (existing) return res.status(400).json({ msg: "Email already exists" });
+
+    const hashed = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashed });
+    await user.save();
+
+    res.status(201).json({ msg: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+require("dotenv").config(); // Load .env first
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("API is working ✅");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
