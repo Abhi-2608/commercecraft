c3a22331b741b898366ead8b70a84fbee821b011
ABHISHEK KUMAR SINGH <abhisinghaks12@gmail.com>
2025-06-12 02:29:35 +0530

    Fix route path to auth.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/auth", require("./config/routes/auth"));
+
 
 
 // Test Route
